3965788cb5221b1cdea6f80d7276c3716d6c92d3
Paul T <PaulTorry@users.noreply.github.com>
2022-12-14 22:34:55 +0000

    prototype

--- src/View/drawView.js
+++ src/View/drawView.js
@@ -1,165 +1,35 @@
-import { Vec } from '../Vec.js'
+// import { Vec } from '../Vec.js'
 import { drawLine, drawTrace, cosineCurve } from '../drawFunctions.js'
+import {Vec} from '../Vec.js'
+/* eslint-disable space-in-parens, no-use-before-define, no-trailing-spaces, no-multiple-empty-lines, comma-spacing, padded-blocks */
 
-// Each slit has a unique colour @TODO get opinions on these
 
-const colours = (i, opacity = 1) => {
+const colours = (i, opacity = 1 ) => {
   const colourArray = [[73, 137, 171], [73, 171, 135], [73, 171, 96], [135, 171, 73], [171, 166, 73], [171, 146, 73]]
   const col = 'rgba(' + colourArray[i][0] + ',' + colourArray[i][1] + ',' + colourArray[i][2] + ',' + opacity + ')'
   return col
 }
 
-// Draw foreground, the rays and sin waves and phasors
 
-function drawForground (c, slit, ray, wave, pos, viewScale, { amp, scale, switchZoom }) {
-  const geo = ray.geo
-  const edges = ray.grating.edges.map(a => a) // .reverse().map(([a, b]) => [a + 2 * ray.grating.firstSlit, b + 2 * ray.grating.firstSlit])
+// function drawForground (c, phasors, pos) {
+//   console.log('agquifg')
+//   c.fillStyle = 'lightgrey'
+//   c.strokeStyle = 'black'
+//   drawLine(c, ...pos.componentXY, ...phasors[0])
+//   drawLine(c, ...pos.componentXY, ...phasors[1])
 
-  const firstSlitPosY = slit.firstSlit + pos.topViewXY.y / 2
-  // console.log(edges.flat(), ray.grating.edges.flat())
-  const screenDisplacement = geo.d + pos.topViewXY.y / 2
-  // c.clearRect(0, 0, c.canvas.width, c.canvas.height)
+// }
 
-  // line from center of slits to screen
-  drawLine(c, pos.grating.x, pos.topViewXY.y / 2, geo.D, geo.d)
+// function drawBackground (c , pos) {
+//   // c.clearRect(0, 0, c.canvas.width, c.canvas.height)   - for canvas based optimisation
+//   c.fillStyle = 'lightgrey'
+//   c.strokeStyle = 'black'
+//   // Draws the areas on screen 
+//   c.strokeRect(0, 0, c.canvas.width, c.canvas.height)
+//   // c.strokeRect(0, 0, ...pos.componentXY)
+//   // c.strokeRect(pos.componentXY.x, 0, pos.componentXY.dx, pos.componentXY.y)
 
-  // waves arriving at grating
-  cosineCurve(c, wave, pos.grating.x, slit.firstSlit + pos.topViewXY.y / 2, [-pos.grating.x, pos.grating.x])
-
-  // waves, phasors at slit and at path difference
-  let arrowStart = new Vec(0, 0)
-
-  // getSinFill is an array to pass to drawSin which draws the red and blue components on the wave
-  // Work in progress const getSinFill = (a, b) => [[a, b - a, 'blue', (a) => Math.max(a, 0)], [a, b - a, 'red', (a) => Math.min(a, 0)]]
-  const getSinFill = (aa, bb) => {
-    const a = aa; const b = bb - aa
-    return [[a, b, 'blue', (a) => Math.max(a, 0)], [a, b, 'red', (a) => Math.min(a, 0)]]
-  }
-
-  /*
-  *   The main loop which goes through each edge to draw the sin cures, and phasors (also the phasors at bottom right)
-  */
-
-  edges.forEach(([top, bot], i) => {
-    const slitTop = new Vec(pos.grating.x, top + firstSlitPosY)
-    const slitBottom = new Vec(pos.grating.x, bot + firstSlitPosY)
-
-    // sincurves at angles
-    cosineCurve(c, wave, ...slitTop, [0, geo.l / 2], 0, 1, geo.theta, colours(i, 0.4))
-    cosineCurve(c, wave, ...slitBottom, [0, geo.l / 2], 0, 1, geo.theta, colours(i), getSinFill(-top * geo.sin, -bot * geo.sin))
-
-    // phasor at grating
-    drawLine(c, ...slitTop, ...ray.phasorAtGrating.scale(wave.amplitude))
-
-    // on angled sin curve
-    // drawLine(c, ...slitTop.add(p1), ...ph1.scale(wave.amplitude))
-    // drawLine(c, ...slitBottom.add(p2), ...ph2.scale(wave.amplitude))
-    drawLine(c, ...slitTop.add(ray.calcPhasorPos(top)), ...ray.calculatePhasor(top).scale(wave.amplitude))
-    drawLine(c, ...slitBottom.add(ray.calcPhasorPos(bot)), ...ray.calculatePhasor(bot).scale(wave.amplitude))
-
-    // Phasors at botom and in sum
-    const integral = ray.calcIntegral(top, bot)
-    // vector at bottom
-    drawLine(c, ...pos.phaseDiagram.addXY(-100, i * 40 - slit.number * 20 + 20), ...integral.scale(wave.amplitude), colours(i))
-    // vector added to sum
-    drawLine(c, ...arrowStart.add(pos.phaseDiagram), ...integral.scale(wave.amplitude), colours(i))
-    arrowStart = arrowStart.add(integral.scale(wave.amplitude))
-
-    // const integralPh = ph.integrateTo(ph2).scale(5 / (slit.width * geo.sin))
-    drawLine(c, ...pos.phaseDiagram.addXY(-100, i * 40 - slit.number * 20 + 20), ...integral.scale(wave.amplitude), colours(i))
-  })
-
-  // bottom wave with areas
-  // const fills = sd.centres.map((yy, i, a) => [yy * geo.sin, 3, colours(i)])
-
-  if (switchZoom) {
-    let { length, phase, amplitude } = wave
-    length = length / (geo.sin * -4)
-    const wave2 = { length, phase, amplitude }
-    const fills = slit.edges.map((c, i, a) => {
-      const [yyy, yy] = c.map(cc => cc * -1 / 4)
-      return [Math.min(-yyy, -yy), Math.max(Math.abs(-yyy + yy), 1), colours(i)]
-    })
-    cosineCurve(c, wave2, 300, pos.phaseDiagram.y, [-150, 500], 0, 4, 0, 'black', fills)
-  } else {
-    const fills = slit.edges.map((c, i, a) => {
-      const [yyy, yy] = c.map(cc => cc * geo.sin)
-      return [Math.min(-yyy, -yy), Math.max(Math.abs(-yyy + yy), 1), colours(i)]
-    })
-    cosineCurve(c, wave, 300, pos.phaseDiagram.y, [-150, 500], 0, 4, 0, 'black', fills)
-    drawLine(c, 300, 600, 0, 200, 'black')
-  }
-
-  let resultAmpitude = ray.modulatedResultant.mag * wave.amplitude
-  let wavePhasor = ray.modulatedResultant.scale(wave.amplitude * viewScale.intensity)
-
-  if (amp) {
-    resultAmpitude = resultAmpitude * ray.modulatedResultant.mag
-    wavePhasor = wavePhasor.scale(ray.modulatedResultant.mag)
-  }
-
-  if (!scale) {
-    const scaleFactor = scale ? 1 : slit.number * (slit.realWidth || 20) / 50
-    resultAmpitude = resultAmpitude * scaleFactor
-    wavePhasor = wavePhasor.scale(scaleFactor)
-  }
-
-  drawLine(c, ...pos.phaseDiagram.addXY(100, 0), ...ray.modulatedResultant.scale(wave.amplitude * slit.number), 'black')
-
-  // Resultant sin wave and phasor at right
-  const newWave2 = { amplitude: resultAmpitude * viewScale.intensity, length: wave.length, phase: ray.modulatedResultant.phase - Math.PI / 2 }
-  cosineCurve(c, newWave2, pos.screen.x, screenDisplacement, [0, wave.phase * wave.length], 0, 1, 0, 'black')
-  drawLine(c, pos.screen.x, screenDisplacement, ...wavePhasor, 'black')
-}
-
-// The grating is drawn by rectangles, this function takes the edges of the slit, the top and bottom and makes pairs oy y-coords
-function drawVerticalGrating (c, { edges: e, firstSlit: f, width: w }, length, xpos, thickness) {
-  // Add the top and bottom of the grating  flatten to a set of pairs for drawing
-  [0].concat(e.flat().map((v) => v + f + length / 2)).concat([length])
-    .reduce((ac, cv, i, ar) => i % 2 ? ac.concat([[ar[i - 1], ar[i]]]) : ac, [])
-    // draw each pair as a rectangle
-    .forEach(([y1, y2], i, a) => { c.fillRect(xpos - thickness, y1, thickness * 2, y2 - y1) })
-}
-
-function drawHorizontalGrating (c, { edges: e, firstSlit: f, width: w }, longStart, length, transStart, thickness, horizontal, centered) {
-  // Add the top and bottom of the grating  flatten to a set of pairs for drawing
-  const flattenedEdges = [longStart].concat(e.flat().map((v) => v + longStart + 100)).concat([length + longStart])
-    .reduce((ac, cv, i, ar) => i % 2 ? ac.concat([[ar[i - 1], ar[i]]]) : ac, [])
-    // draw each pair as a rectangle
-  flattenedEdges.forEach(([x1, x2], i, a) => { c.fillRect(x1, transStart - thickness, x2 - x1, thickness * 2) })
-}
-
-/*
-*  Draws the areas for each section, the screen and the grating
-*/
-
-function drawBackground (c, intensity, pos, amplitude, slit, { scale, show, amp, switchZoom }, viewScale) {
-  // c.clearRect(0, 0, c.canvas.width, c.canvas.height)   - for canvas based optimisation
-  c.fillStyle = 'lightgrey'
-  c.strokeStyle = 'black'
-  // Draws the areas on screen
-  c.strokeRect(0, 0, c.canvas.width, c.canvas.height)
-  c.strokeRect(0, 0, ...pos.topViewXY)
-  c.strokeRect(pos.screen.x, 0, pos.screen.dx, pos.topViewXY.y)
-
-  // Draws the grating
-  drawVerticalGrating(c, slit, pos.topViewXY.y, pos.grating.x, pos.grating.dx)
-  if (switchZoom) {
-    drawHorizontalGrating(c, slit, 200, pos.phaseDiagram.y - 100, pos.topViewXY.y + 10, pos.grating.dx, true)
-  }
-  // Draw the intensity traces
-
-  const traceAmplitude = amplitude * viewScale.intensity
-  // console.log(slit.width, typeof slit.width)
-  const scaleFactor = scale ? 1 : slit.number * (slit.realWidth || 20) / 50
-
-  if (show) {
-    drawTrace(c, intensity[0], pos.screen.x, 0, 'rgba(255, 0, 0, 0.3)', 0, 1, -traceAmplitude, 0, amp, scaleFactor)
-    drawTrace(c, intensity[1], pos.screen.x, 0, 'rgba(0, 255, 0, 0.5)', 0, 1, -traceAmplitude, 0, amp, scaleFactor)
-  }
-  drawTrace(c, intensity[2], pos.screen.x, 0, 'black', 0, 1, -traceAmplitude, 0, amp, scaleFactor)
-  drawTrace(c, intensity[3], pos.screen.x, 0, 'rgba(0, 0, 0, 0.2)', 0, 1, -traceAmplitude, 0, amp, scaleFactor)
-  drawTrace(c, intensity[4], pos.screen.x - 100, 0, undefined, 0, 1, -traceAmplitude, 0, amp, scaleFactor)
-}
+  
+// }
 
 export { drawForground, drawBackground }
--- src/phasorSim.js
+++ src/phasorSim.js
@@ -1,12 +1,14 @@
 /* global requestAnimationFrame */
-
 import { Vec } from './Vec.js'
-import { Grating, Ray, IntensityPattern } from './Model/OpticsClasses.js'
-import { drawForground, drawBackground } from './View/drawView.js'
+// import { drawLine } from './drawFunctions.js'
+// import { drawForground, drawBackground } from './View/drawView.js'
 
+console.log('start')
+const phasors = [Vec.unitY.scale(100), Vec.unitX.scale(100)]
+let wavelength = 20;
 const canvas = document.querySelector('#screen')
 const cx = canvas.getContext('2d')
-let dropdown = document.getElementById('wavelengthSlide')
+// let dropdown = document.getElementById('wavelengthSlide')
 
 const settings = {
   animate: { run: true, notPaused: true },
@@ -14,47 +16,92 @@ const settings = {
 }
 const pos = {
   componentXY: new Vec(200, 200),
-  resultXY: { x: 300, dx: 5 },
+  resultXY: { x: 300, dx: 5 }
 }
+
+const phasorBase = (n = 2) => { return pos.componentXY.scaleXY(1, n) }
+
+console.log(pos.componentXY)
+console.log(...pos.componentXY)
 
 addEventListeners()
 requestAnimationFrame(animateIt)
 update()
 
+function drawForground (c, phasors, pos) {
+  c.fillStyle = 'lightgrey'
+  c.strokeStyle = 'black'
+  drawLine(c, ...phasorBase(1), ...phasors[0])
+  drawLine(c, ...phasorBase(2), ...phasors[1])
+  c.strokeStyle = 'red'
+  drawLine(c, ...phasorBase(3), ...phasors[0])
+  c.strokeStyle = 'green'
+  drawLine(c, ...phasorBase(3).add(phasors[0]), ...phasors[1])
+  c.strokeStyle = 'black'
+  drawLine(c, ...phasorBase(3).add(phasors[0]).add(phasors[1]), ...phasors[1].add(phasors[0]).invert())
+  // console.log(0, 200, phasors[1].mag, phasors[1].theta, 400, 20, 'lightgrey');
+  drawSine(...phasorBase(1), phasors[0].mag, phasors[0].phase, 800, wavelength, 'red')
+  drawSine(...phasorBase(2), phasors[1].mag, phasors[1].phase, 800, 20, 'green')
+  drawResult (...phasorBase(3), phasors[1].mag, 20, phasors[1].phase, phasors[0].mag, wavelength, phasors[0].phase, 800,  'black')
+}
+
+function drawBackground (c, pos) {
+  c.fillStyle = 'lightgrey'
+  c.strokeStyle = 'black'
+  c.strokeRect(0, 0, c.canvas.width, c.canvas.height)
+}
+
+function dragEvent (mouseCoords, b) {
+  phasors[0] = b.subtract(pos.componentXY)
+  update()
+}
+
+function drawSine (x1, y1, A, theta, length, wl, color = 'black') {
+  let fun = (xxx) => A * Math.sin(xPosToTheta(xxx))
+  function xPosToTheta (x) {
+    return (x - 0) / (wl) - theta
+  }
+  iteratePath(x1, y1, length, color, fun)
+}
+
+function drawResult (x1, y1, A, wl, theta, A2, wl2, theta2, length, color = 'black') {
+  let fun = (xxx) => (A * Math.sin(xPosToTheta1(xxx))) + (A2 * Math.sin(xPosToTheta2(xxx)))
+  function xPosToTheta1 (x) {
+    return (x - 0) / (wl) - theta
+  }
+  function xPosToTheta2 (x) {
+    return (x - 0) / (wl2) - theta2
+  }
+  iteratePath(x1, y1, length, color, fun)
+}
+
+function iteratePath (x1, y1, length, color = 'black', func = Math.sin) {
+  if (color) { cx.strokeStyle = color }
+  cx.beginPath()
+  cx.moveTo(x1, y1)
+  for (var xx = x1; xx <= x1 + length; xx += 1) {
+    cx.lineTo(xx, y1 + func(xx))
+  }
+  cx.stroke()
+}
+
+function drawLine (c, x1, y1, dx, dy, color) {
+  if (color) { c.strokeStyle = color }
+  c.beginPath()
+  c.moveTo(x1, y1)
+  c.lineTo(x1 + dx, y1 + dy)
+  c.stroke()
+  c.fill()
+  c.beginPath()
+}
+
 function addEventListeners () {
   let mouseCoords
 
   document.addEventListener('keypress', (e) => {
-    if (e.key === 'a') 
-    if (e.key === 'p') 
-    console.log(wave.amplitude)
+     if (e.key === '=') {wavelength++}
+     if (e.key === '-') {wavelength--}
     update(true)
-  })
-
-  function dragEvent (a, b) {
-    // console.log(a,b);
-    const d = b.subtract(a)
-    if (d.x * d.x > 16 * d.y * d.y || a.x < pos.grating.x || a.x > pos.screen.x || a.y > pos.topViewXY.y) {
-      wave.phase += (d.x) * 0.5 / wave.length
-    } else if (16 * d.x * d.x < d.y * d.y) {
-      displacement += d.y
-      if (displacement > -1 && settings.mirror) displacement = -1
-      if (settings.record) { intensity.addIntensity(ray, undefined, settings.mirror) }
-      if (settings.animate.run && !settings.record) { wave.phase = 0 }
-    }
-    update()
-  }
-  buttons.record.addEventListener('click', (e) => {
-    intensity.recordIntensites()
-    update()
-  })
-  sliders.wave.s.addEventListener('input', (e, v = sliders.wave.s.valueAsNumber) => {
-    if (v !== wave.length) {
-      sliders.wave.t.textContent = v
-      wave.length = v
-      intensity.staleintensities()
-      update(true)
-    }
   })
 
   window.addEventListener('touchstart', ({ touches: [e] }) => { mouseCoords = new Vec(e.pageX, e.pageY); settings.animate.notPaused = false })
@@ -71,17 +118,9 @@ function addEventListeners () {
 
   window.addEventListener('mousedown', e => { mouseCoords = new Vec(e.offsetX, e.offsetY); settings.animate.notPaused = false })
   window.addEventListener('mouseup', e => { mouseCoords = undefined; settings.animate.notPaused = true })
-  window.addEventListener('dblclick', e => {
-    settings.animate.run = !settings.animate.run
-    checkboxes.animate.checked = settings.animate.run
-  })
+  window.addEventListener('dblclick', e => { settings.animate.run = !settings.animate.run })
   window.addEventListener('click', e => {
-    if (e.detail === 3) {
-      settings.record = !settings.record
-      checkboxes.record.checked = settings.record
-      settings.animate.run = false
-      checkboxes.animate.checked = false
-    }
+    if (e.detail === 3) { settings.animate.run = false }
   })
   window.addEventListener('mousemove', (e) => {
     if (mouseCoords) {
@@ -93,21 +132,23 @@ function addEventListeners () {
 }
 
 function update () {
-
   cx.clearRect(0, 0, cx.canvas.width, cx.canvas.height)
-  drawBackground(cx)
-  drawForground(cx)
+  drawBackground(cx, phasors, pos)
+  drawForground(cx, phasors, pos)
 }
 
 function animateIt (time, lastTime) {
   if (lastTime != null & settings.animate.run & settings.animate.notPaused) {
-    wave.phase += (time - lastTime) * 0.002
+    // wave.phase += (time - lastTime) * 0.002
     // const newRay = ray.updatePhase(wave.phase)
     // if (ray.normalisedResultant.phase > newRay.normalisedResultant.phase) {
     //   intensity.addIntensity(ray, undefined, settings.mirror)
     // }
-    // update()
+    phasors.forEach((v, i, a) => { phasors[i] = phasors[i].rotate(-0.01) })
+
+    update()
   }
+
   requestAnimationFrame(newTime => animateIt(newTime, time))
 }
 
